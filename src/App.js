64ec005ef60aafe7dd8d8eb2bf417bbafb777a1e
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-08 16:54:44 +0200

    create navbar with register button and log in button, add simple css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Header from "./components/layout/Header";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
         </Switch>
---
+++ src/components/auth/AuthOptions.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const AuthOptions = () => {
+  const history = useHistory();
+
+  const register = () => history.push("/register");
+  const login = () => history.push("/login");
+  return (
+    <nav className="auth-options">
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login in</button>
+    </nav>
+  );
+};
+
+export default AuthOptions;

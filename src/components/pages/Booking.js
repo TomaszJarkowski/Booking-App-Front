3dd5d4824d45c26826ac5849455d0ce07bc44c5f
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-13 00:49:03 +0200

    create calendar picker

--- src/components/pages/Booking.js
+++ src/components/pages/Booking.js
@@ -1,14 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Route, Redirect } from "react-router-dom";
 import UserContext from "../../context/UserContext";
-
+import Calendary from "../layout/Calendary";
 const Booking = () => {
   const { userData } = useContext(UserContext);
+  const BookingPage = () => (
+    <div className="page-booking">
+      <div className="container">
+        <h1>Booking</h1>
+        <h2>Select the day and time and we will reserve a table for you ❤</h2>
+        <Calendary />
+      </div>
+    </div>
+  );
 
   return (
     <Route
       render={() =>
-        userData.user ? <h3>Booking</h3> : <Redirect to="/login" />
+        userData.user ? <BookingPage /> : <Redirect to="/login" />
       }
     />
   );

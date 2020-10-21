f174ba11f200d7e2f616cf01c8c070fe208ded0d
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-21 13:52:23 +0200

    Updates

---
+++ precache-manifest.dbd399cd1706d3aae797b52bef4752c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722f8c81ff973994b57263c2dac3a735",
+    "url": "/Booking-App-Front/index.html"
+  },
+  {
+    "revision": "4079ffdc440e9d321758",
+    "url": "/Booking-App-Front/static/css/main.8f460c35.chunk.css"
+  },
+  {
+    "revision": "7fd058dcac0a577cb291",
+    "url": "/Booking-App-Front/static/js/2.5ccdc169.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Booking-App-Front/static/js/2.5ccdc169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4079ffdc440e9d321758",
+    "url": "/Booking-App-Front/static/js/main.e6872cd5.chunk.js"
+  },
+  {
+    "revision": "8317e263f5bd8c7bcf96",
+    "url": "/Booking-App-Front/static/js/runtime-main.eb5230d1.js"
+  },
+  {
+    "revision": "59156c30a22442257645aaf95a6128a9",
+    "url": "/Booking-App-Front/static/media/404.59156c30.svg"
+  },
+  {
+    "revision": "16babcc0c18579a3e8451a17eb483483",
+    "url": "/Booking-App-Front/static/media/undraw_Booking_re_gw4j.16babcc0.svg"
+  }
+]);
